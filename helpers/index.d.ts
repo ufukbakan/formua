2ca9fe2480cb954c77d3f600196b52312b81fc7b
Ufuk Bakan <println.ufukbakan@gmail.com>
2024-02-25 05:59:13 +0300

    feat: type fixes & improvements for validators & transformers

--- helpers/index.d.ts
+++ helpers/index.d.ts
@@ -1,7 +1,9 @@
+type IndependentValidator<T> = (value: T) => boolean;
+type DependentValidator<T> = (fields: Record<string, string | boolean>, value: T) => boolean
+
 // validation helpers:
-type booleanReturner = (o: any) => boolean;
-export function chainAnd(...callbacks: booleanReturner[]): booleanReturner;
-export function chainOr(...callbacks: booleanReturner[]): booleanReturner;
+export function chainAnd<T>(...callbacks: IndependentValidator<T>[]): IndependentValidator<T>;
+export function chainOr<T>(...callbacks: IndependentValidator<T>[]): IndependentValidator<T>;
 export function isEmail(val: any): boolean;
 export function isPhoneNumber(val: any): boolean;
 export function isUrl(val: any): boolean;
@@ -16,27 +18,27 @@ export function isLowercase(val: any): boolean;
 export function isUppercase(val: any): boolean;
 export function isInteger(val: any): boolean;
 export function isFloat(val: any): boolean;
-export function hasNoSpaces(val: any): boolean;
-export function hasNoSpecialCharacters(val: any): boolean;
-export function minLength(n: number): booleanReturner;
-export function minTrimmedLength(n: number): booleanReturner;
-export function maxLength(n: number): booleanReturner;
-export function maxTrimmedLength(n: number): booleanReturner;
-export function sameAs(val: any): booleanReturner;
-export function sameAsField(fieldName: string): booleanReturner;
-export function equals(val: any): booleanReturner;
-export function equalsToField(fieldName: string): booleanReturner;
-export function required(val: any): boolean;
+export function hasNoSpaces(val: string): boolean;
+export function hasNoSpecialCharacters(val: string): boolean;
+export function minLength(n: number): IndependentValidator<string>;
+export function minTrimmedLength(n: number): IndependentValidator<string>;
+export function maxLength(n: number): IndependentValidator<string>;
+export function maxTrimmedLength(n: number): IndependentValidator<string>;
+export function sameAs<T>(val: any): IndependentValidator<T>;
+export function sameAsField<T>(fieldName: string): DependentValidator<T>;
+export function equals<T>(val: any): IndependentValidator<T>;
+export function equalsToField<T>(fieldName: string): DependentValidator<T>;
+export function required<T>(val: any): boolean;
 
 // transform helpers:
 type valueReturner = (o: any) => any;
 export function chainTransform(...transformations: valueReturner[]): valueReturner;
-export function toUpper(val: string): any;
-export function toLocaleUpper(val: string): any;
-export function toLower(val: string): any;
-export function toLocaleLower(val: string): any;
-export function trim(val: string): any;
-export function trimEnd(val: string): any;
-export function trimStart(val: string): any;
+export function toUpper(val: string): string;
+export function toLocaleUpper(val: string): string;
+export function toLower(val: string): string;
+export function toLocaleLower(val: string): string;
+export function trim(val: string): string;
+export function trimEnd(val: string): string;
+export function trimStart(val: string): string;
 export function prefix(val: any): valueReturner;
 export function postfix(val: any): valueReturner;
